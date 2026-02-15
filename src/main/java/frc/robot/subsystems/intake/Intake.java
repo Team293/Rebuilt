2b301d6998f0c601d75d4af0c3b44070c65a6229
Sparsh <64652370+ImSpxrsh@users.noreply.github.com>
2026-02-15 17:43:44 -0500

    Implement ASynch Refresher

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -4,9 +4,9 @@ import frc.lib.subsystem.SpikeSystem;
 import frc.robot.subsystems.drive.CommandSwerveDrivetrain;
 
 public class Intake extends SpikeSystem<IntakeIO.IntakeIOInputs> {
-     private static final double BASE_SPEED = 1;//add in accurate numbers when protoype built
-     private static final double SPEED_PER_MPS = 0.05;
-     private static final double MAX_SPEED = 5.0;
+     private static final double BASE_SPEED = 1; // Rotation Per Second  
+     private static final double SPEED_PER_MPS = 0.05; //speed per m/s of drive velocity
+     private static final double MAX_SPEED = 5.0; //  speed cap
 
     private final IntakeIOTalonFX intakeIO;
     private final CommandSwerveDrivetrain drivetrain;
@@ -19,34 +19,39 @@ public class Intake extends SpikeSystem<IntakeIO.IntakeIOInputs> {
         this.intakeIO = new IntakeIOTalonFX();
     }
 
-    @Override
+     // Scales intake speed dependent of drivetrain speed 
+     @Override
     public void onPeriodic() {
         if (running) {
-            double driveSpeed = Math.abs(drivetrain.getState().Speeds.vxMetersPerSecond);
-            double speed = Math.min(BASE_SPEED + driveSpeed * SPEED_PER_MPS, MAX_SPEED); 
+             double driveSpeed = Math.abs(drivetrain.getState().Speeds.vxMetersPerSecond);
+             double speed = Math.min(BASE_SPEED + driveSpeed * SPEED_PER_MPS, MAX_SPEED);
             intakeIO.on(speed);
         }
     }
 
+     // Enables the intake 
      public void on() {
         running = true;
     }
 
+     // Disables the intake 
      public void off() {
         running = false;
         intakeIO.off();
     }
 
+     // Deploys the OTB intake 
      public void deploy() {
         intakeIO.deploy();
     }
 
+     // Retracts the intake back to its stored position
      public void retract() {
         intakeIO.retract();
     }
 
     @Override
-    protected Runnable setupDataRefresher() { //Spikelib 
-        return useDataRefresher(intakeIO);
+    protected Runnable setupDataRefresher() {
+        return useAsyncDataRefresher(intakeIO);
     }
 }
