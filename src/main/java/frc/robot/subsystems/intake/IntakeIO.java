dba6a1d36ac4657b7ce313209fee73ba33354942
Sparsh <64652370+ImSpxrsh@users.noreply.github.com>
2026-02-15 17:44:23 -0500

    Clarify comments in IntakeIOInputs

--- src/main/java/frc/robot/subsystems/intake/IntakeIO.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO.java
@@ -8,11 +8,11 @@ public interface IntakeIO extends BaseIO<IntakeIO.IntakeIOInputs> {
 
     @AutoLog
     class IntakeIOInputs extends BaseInputClass {
-        public double intakeVelocityRPS = 0.0;
-        public double intakeCurrentAmps = 0.0; // add in values
-        public double deployVelocityRPS = 0.0;
-        public double deployCurrentAmps = 0.0;
-        public boolean deployed = false;
+        public double intakeVelocityRPS = 0.0; // Rotations per second
+  //      public double intakeCurrentAmps = 0.0; // Amps
+        public double deployVelocityRPS = 0.0; // Rotations per second
+  //      public double deployCurrentAmps = 0.0;
+        public boolean deployed = false; // whether the intake is deployed
     }
 
     void on(double speed);
