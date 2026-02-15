a95d4ddd02e4a268447fda031435159ac58e21cb
Justin E <jusedgington100@gmail.com>
2026-02-14 21:08:33 -0500

    Use 250Hz odometry frequency

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -197,7 +197,7 @@ public class TunerConstants {
      */
     public static CommandSwerveDrivetrain createDrivetrain() {
         return new CommandSwerveDrivetrain(
-            DrivetrainConstants, FrontLeft, FrontRight, BackLeft, BackRight
+            DrivetrainConstants, 250.0, FrontLeft, FrontRight, BackLeft, BackRight
         );
     }
 
