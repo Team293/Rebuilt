359d300480d07095d4e688eda7250b1d19c9a11a
Sparsh <64652370+ImSpxrsh@users.noreply.github.com>
2026-02-22 12:48:10 -0500

    12 intake initial code (#18)
    * Add intake subsystem with CAN ID and motor control
    
    * Updated Intake Code; Double Motors; Deployment & Retract;  intake speed in relation to drivetrains speed
    
    * Add intake motor IDs to CanID enum
    
    * Deployment Configurations
    
    * Change StatusSignal types to use units
    
    * added comments moved away from DutyCycleOut
    
    * Implement ASynch Refresher
    
    * Clarify comments in IntakeIOInputs
    
    * Uncomment current variables in IntakeIOInputs
    
    * removed all DutyCycleOut mentions
    
    * Stop intake motor when retracted
    
    * Remove unnecessary files, reformat, and update intake deploying limits
    
    * Add controller commands for intake
    
    * Add controller commands for intake
    
    * fixed order of ops on data fetcher and misleading comment
    
    * fixed can id
    
    ---------
    
    Co-authored-by: nathan <incbom.dev@gmail.com>
    Co-authored-by: Nathan <99501939+NathanEdg@users.noreply.github.com>
    Co-authored-by: Trevor <65660736+realtrev@users.noreply.github.com>

--- src/main/java/frc/robot/CanID.java
+++ src/main/java/frc/robot/CanID.java
@@ -4,9 +4,11 @@ package frc.robot;
  * Holder for all CAN device IDs besides drivetrain devices
  */
 public enum CanID {
+    INTAKE_MOTOR(9),
+    INTAKE_DEPLOY_MOTOR(11),
     INDEXER(10);
 
-    private int deviceID;
+    private final int deviceID;
 
     CanID(int deviceID) {
         this.deviceID = deviceID;
---
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -0,0 +1,148 @@
+package frc.robot.subsystems.intake;
+
+import frc.lib.subsystem.SpikeSystem;
+import frc.robot.subsystems.drive.CommandSwerveDrivetrain;
+
+public class Intake extends SpikeSystem<IntakeIO.IntakeIOInputs> {
+    private static final double DEPLOY_SPEED = 1.0; // Speed in Rotations Per Second to deploy the intake
+    private static final double RETRACT_SPEED = -1.0; // Speed in Rotations Per Second to retract the intake
+    private static final double DEPLOY_CURRENT_THRESHOLD = 10.0; // Amp limit for the deploy motor. Watches
+                                                                 // for a resistance to the motor to see when it's
+                                                                 // deployed
+    private static final double BASE_SPEED_INTAKE = 1; // Rotation Per Second
+    private static final double SPEED_PER_MPS = 0.05; // Speed added to the base speed per m/s of drive velocity
+    private static final double MAX_SPEED = 5.0; // speed cap, max speed of the intake in RPS
+
+    public enum IntakeState {
+        DEPLOYED, RETRACTED, DEPLOYING, RETRACTING
+    }
+
+    private IntakeIOTalonFX intakeIO;
+    private final CommandSwerveDrivetrain drivetrain;
+
+    private boolean running = false; // True if the intake is running, False otherwise
+
+    // Intake constructor
+    public Intake(CommandSwerveDrivetrain drivetrain) {
+        super("Intake", new IntakeIO.IntakeIOInputs());
+        this.drivetrain = drivetrain;
+        enable();
+    }
+
+    // Intake periodic function
+    // Increases intake speed when driving faster
+    @Override
+    public void onPeriodic() {
+        // Only act on the intake motor if the intake is running
+        if (running) {
+            switch (intakeIO.getIntakeState()) {
+                case DEPLOYING: // Intake is deploying
+                    doDeployingState();
+                    break;
+                case RETRACTING: // Intake is retracting
+                    doRetractingState();
+                    break;
+                case DEPLOYED: // Intake is fully deployed
+                    doDeployedState();
+                    break;
+                case RETRACTED: // Intake is fully retracted
+                    doRetractedState();
+                    break;
+                default:
+                    // Error, unknown state!
+                    // Turn off the deploy and intake motors!
+                    intakeIO.setDeploySpeed(0.0);
+                    intakeIO.setIntakeSpeed(0.0);
+                    // Log unknown state
+                    break;
+            }
+        }
+    }
+
+    // Run the DEPLOYING state periodic actions
+    private void doDeployingState() {
+        // Make sure the intake motor is OFF
+        intakeIO.setIntakeSpeed(0.0);
+        // Set intake speed to deploy
+        intakeIO.setDeploySpeed(DEPLOY_SPEED);
+        // Check to see if the intake has fully deployed
+        if (intakeIO.getDeployMotorCurrent() > DEPLOY_CURRENT_THRESHOLD) { // If the deploy motor current exceeds the
+                                                                           // threshold, we can assume it's fully
+                                                                           // deployed
+            intakeIO.setDeploySpeed(0.0); // Disable the deploy motor
+            intakeIO.setIntakeState(IntakeState.DEPLOYED); // Mark as deployed
+        }
+    }
+
+    // Run the DEPLOYED state periodic actions
+    private void doDeployedState() {
+        // Get the absolute velocity of the entire robot
+        double robotAbsoluteVelocity = Math.abs(drivetrain.getState().Speeds.vxMetersPerSecond);
+
+        // Adjust the intake speed, increase the intake as the robot moves faster
+        // Cap at MAX_SPEED
+        double speed = Math.min(BASE_SPEED_INTAKE + robotAbsoluteVelocity * SPEED_PER_MPS, MAX_SPEED);
+
+        // Update the intakeIO on speed
+        intakeIO.setIntakeSpeed(speed);
+    }
+
+    // Run the RETRACTING state periodic actions
+    private void doRetractingState() {
+        // Make sure the intake motor is OFF
+        intakeIO.setIntakeSpeed(0.0);
+        // Set the intake speed to retract
+        intakeIO.setDeploySpeed(RETRACT_SPEED);
+        // Check to see if the intake has fully retraced
+        if (intakeIO.getDeployMotorCurrent() > DEPLOY_CURRENT_THRESHOLD) { // If the deploy motor current exceeds the
+                                                                           // threshold, we can assume it's fully
+                                                                           // retracted
+            intakeIO.setDeploySpeed(0.0); // Disable the deploy motor
+            intakeIO.setIntakeState(IntakeState.RETRACTED); // Mark as retracted
+        }
+    }
+
+    // Run the RETRACTED state periodic actions
+    private void doRetractedState() {
+        // Intake is fully deployed or retracted
+        // Make sure intake motor is OFF
+        intakeIO.setIntakeSpeed(0.0);
+
+        // Make sure deploy motor is OFF
+        intakeIO.setDeploySpeed(0.0);
+    }
+
+    // Enables the intake
+    public void enable() {
+        running = true;
+    }
+
+    // Disables the intake
+    public void disable() {
+        running = false;
+
+        // Turn off the intake motor
+        intakeIO.setIntakeSpeed(0.0);
+        intakeIO.setDeploySpeed(0.0);
+    }
+
+    // Deploys the over the bumper intake
+    public void deploy() {
+        if (intakeIO.getIntakeState() != IntakeState.DEPLOYED) { // Only try to deploy if we aren't already deployed
+            intakeIO.setIntakeState(IntakeState.DEPLOYING); // Mark as deploying
+        }
+    }
+
+    // Retracts the intake back to its stored position
+    public void retract() {
+        if (intakeIO.getIntakeState() != IntakeState.RETRACTED) { // Only try to retract if we aren't already retracted
+            intakeIO.setIntakeState(IntakeState.RETRACTING); // Mark as retracting
+        }
+    }
+
+    @Override
+    protected Runnable setupDataRefresher() {
+        intakeIO = new IntakeIOTalonFX();
+        return useAsyncDataRefresher(intakeIO);
+    }
+}
