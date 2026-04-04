78c5be7745a5f8222bd94c62213616624b44ad8e
Trevor <tgavdaly@gmail.com>
2026-04-04 11:09:51 -0400

    Add LED control to robot

--- src/main/java/frc/robot/subsystems/turret/Turret.java
+++ src/main/java/frc/robot/subsystems/turret/Turret.java
@@ -78,6 +78,7 @@ public class Turret extends SpikeSystem<TurretIO.TurretIOInputs> {
     }
 
     public void toggleAimingOverride() {
+        RobotContainer.getLEDController().switchPreset("fixed");
         this.overrideAutomaticAiming = !this.overrideAutomaticAiming;
     }
 
