81de5913e4e61c2c54b9e8f5f87e7aa6a37620dd
NathanEdg <nathanedgington1@hvrsd.org>
2026-04-04 13:53:27 -0400

    New shot data

--- src/main/java/frc/robot/subsystems/targeting/ShotData.java
+++ src/main/java/frc/robot/subsystems/targeting/ShotData.java
@@ -8,22 +8,29 @@ public class ShotData {
     public static final InterpolatingDoubleTreeMap distanceToTOFConstant = new InterpolatingDoubleTreeMap(); //TOF = Time of Flight
     
     static {
-        distanceToRPM.put(1.0, 1800.0);
-        distanceToHoodAngle.put(1.0, 15.0);
         distanceToTOFConstant.put(1.0, 0.8);
         distanceToTOFConstant.put(4.0, 0.85  );
 
-        distanceToRPM.put(2.0, 2050.0);
-        distanceToHoodAngle.put(2.0, 23.0);
+        distanceToRPM.put(2.0, 2000.0);
+        distanceToHoodAngle.put(2.0, 25.0);
+
+        distanceToRPM.put(2.5, 2150.0);
+        distanceToHoodAngle.put(2.5, 25.0);
 
         distanceToRPM.put(3.0, 2200.0);
-        distanceToHoodAngle.put(3.0, 30.0);
+        distanceToHoodAngle.put(3.0, 25.0);
+
+        distanceToRPM.put(3.5, 2400.0);
+        distanceToHoodAngle.put(3.5, 27.0);
 
         distanceToRPM.put(4.0, 2500.0);
-        distanceToHoodAngle.put(4.0, 32.0);
+        distanceToHoodAngle.put(4.0, 27.0);
 
-        distanceToRPM.put(5.3, 2650.0);
-        distanceToHoodAngle.put(5.3, 35.0);
+        distanceToRPM.put(4.5, 2600.0);
+        distanceToHoodAngle.put(4.5, 27.0);
+
+        distanceToRPM.put(4.9, 2700.0);
+        distanceToHoodAngle.put(4.9, 29.0);
 
         distanceToRPM.put(17.069, 5000.0);
         distanceToHoodAngle.put(17.069, 45.0);
